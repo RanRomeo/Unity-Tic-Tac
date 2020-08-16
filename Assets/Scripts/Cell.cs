90e54795e41da9f6e31b2f284546c65185c4d8e1
Ran <ranzor007@outlook.com>
2020-08-17 01:54:46 +0530

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour
+{
+
+
+    public Text mLabel1;
+    public Button mButton;
+    public Main mMain;
+
+
+    public void Fill()
+    {
+        mButton.interactable = false;
+
+        mLabel1.text = mMain.GetTurnCharacter();
+
+        mMain.Switch();
+
+    }
+
+
+}
